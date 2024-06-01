21eba1f6abda2f687868a5e9ade9c3b87470dc81
Suthar98 <sutharvictor10@gmail.com>
2024-06-02 00:06:11 +0530

    #5, set up the server and login endpoint

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const app = express();
+const port = 3001;
+const secretKey = 'your_secret_key';
+
+app.use(bodyParser.json());
+
+const users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user) {
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
