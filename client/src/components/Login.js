ceb373a8818bf587290ff45a06024f79ff81592c
Suthar98 <sutharvictor10@gmail.com>
2024-06-03 03:16:34 +0530

    #12, add more style to login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import './Login.css';
 import { toast } from 'react-toastify';
+import backgroundImage from '../assets/images/image1.jpg';
+import Logo from '../assets/images/image2.jpg'
 
 
 const Login = () => {
@@ -34,44 +36,54 @@ const Login = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Username</label>
-          <input
-            type="text"
-            placeholder='Enter user name'
-            className="form-control"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Password</label>
-          <div className="input-group">
+    <div
+      className="log__wrapper"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="container1 mt-5">
+        <img
+          className="img-thumbnail mx-auto d-block mb-2"
+          src={Logo}
+          alt="logo"
+        />
+        <div className="h4 mb-2 text-center">Login</div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
             <input
-              type={passwordVisible ? 'text' : 'password'}
+              type="text"
+              placeholder='Enter user name'
               className="form-control"
-              placeholder='Enter Password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required
             />
-
-            <button
-              type="button"
-              className="btn btn-outline-primary btn-password"
-              onClick={togglePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
-            </button>
           </div>
-        </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <div className="input-group">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                className="form-control"
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
 
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+              <button
+                type="button"
+                className="btn btn-outline-primary btn-password"
+                onClick={togglePasswordVisibility}
+              >
+                <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+              </button>
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
