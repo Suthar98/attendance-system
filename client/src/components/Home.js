2e08f2254132f039902d4a58acacf2969f9a70e0
Suthar98 <sutharvictor10@gmail.com>
2024-06-03 13:29:13 +0530

    #13, add more styles to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import authService from '../services/authService';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import './home.css';
+
 
 const Home = () => {
   const navigate = useNavigate();
@@ -11,10 +17,30 @@ const Home = () => {
   };
 
   return (
-    <div className="container">
-      <h2>Welcome Home</h2>
-      <button className="btn btn-primary" onClick={handleLogout}>Logout</button>
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#"><b>Attendance System</b></Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+
+
+            <Form className="d-flex ml-auto">
+              <Button variant="outline-success" onClick={handleLogout}>Logout</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* <Container className="welcome-message">
+        <h1>Welcome to the Attendance System</h1>
+      </Container> */}
+
+      <div className="background-image">
+        <div className="welcome-message">
+          <h1>Welcome to the Attendance System</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
